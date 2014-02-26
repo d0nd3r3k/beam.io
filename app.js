130005d3a57bac9c1434b505c7cae426500b20e7
Donald Derek <donaldderek.haddad@gmail.com>
2014-02-26 19:33:50 +0200

    serial

--- app.js
+++ app.js
@@ -105,15 +105,12 @@ var serialPort = new SerialPort("/dev/ttyUSB0", {
   baudrate: 115200
 }, true); // this is the openImmediately flag [default is true]
 
-serialPort.open(function () {
-  console.log('open');
-  serialPort.on('data', function(data) {
-    console.log('data received: ' + data);
-  });
-  serialPort.write("ls\n", function(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results);
-  });
+serialPort.on('open',function() {
+  //console.log('Port open');
+});
+
+serialPort.on('data', function(data) {
+  console.log(data);
 });
 
 
