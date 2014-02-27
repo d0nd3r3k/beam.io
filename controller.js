72a32a18e9b976b67eca5f4135d88b2c0eaab43e
Donald Derek <donaldderek.haddad@gmail.com>
2014-02-27 09:39:21 +0200

    plz

--- controller.js
+++ controller.js
@@ -46,16 +46,18 @@ exports.sensor = function(type, message, name){
 				if(err) console.log(err)
 				console.log(sensdata)	
 				data = JSON.parse(sensdata);
-				if (type == "temp") value = data.sensors.sht_temperature + " &deg;C";
+				console.log(data.sensors.sht_temperature);
+				/*if (type == "temp") value = data.sensors.sht_temperature + " &deg;C";
 				if (type == "hum") value = data.sensors.sht_humidity + " %";
 				if (type == "illu") value = data.sensors.illuminance + " Lx";
-				if (type == "pressu") value = data.sensors.bmp_pressure + " Pa";
-				message = message + ": " + value;
-				console.log(message);
-				tuwm.post(message+" @"+name, image_path, function(err, response) {
+				if (type == "pressu") value = data.sensors.bmp_pressure + " Pa";*/
+				
+				//message = message + ": " + value;
+				//console.log(message);
+				/*tuwm.post(message+" @"+name, image_path, function(err, response) {
 					if (err) console.log(err);
 					console.log(response)
-				});
+				});*/
 			})
 		} 
 	});
