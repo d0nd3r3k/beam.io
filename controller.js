4c33b41bc375e82e57ff83181df42a5d730e9e5d
Donald Derek <donaldderek.haddad@gmail.com>
2014-02-27 11:00:07 +0200

    asd

--- controller.js
+++ controller.js
@@ -44,7 +44,14 @@ exports.sensor = function(type, message, name){
 			child = exec("python bin/baselisten.py", function(err, stdout, stderr){
 				if(err) console.log(err)
 				console.log(stdout);	
-				data = JSON.parse(stdout);
+				//data = JSON.parse(stdout);
+				data = {"sensors": {
+					    "sht_temperature":  24.34,
+					    "sht_humidity":  46.4,
+					    "illuminance": 0,
+					    "bmp_pressure":  1016.16,
+					    "bmp_temperature":  24.3
+					}}
 				console.log(data.sensors.bmp_temperature);
 				if (type == "temp") value = data.sensors.sht_temperature + " &deg;C";
 				if (type == "hum") value = data.sensors.sht_humidity + " %";
